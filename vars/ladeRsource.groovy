43040a6a84043a42c834392419fd9c8d19b425a6
carsten sukatsch <121088867+carstenSukatsch@users.noreply.github.com>
2023-05-20 20:52:51 +0200

    Update ladeRsource.groovy

--- vars/ladeRsource.groovy
+++ vars/ladeRsource.groovy
@@ -1,6 +1,6 @@
 def call( Map cfg =[:] ){
   def scrContent = libraryResource("data/myTx.txt")
   def wert = "blah balh"
-   scrContent.replaceAll( 'kk', '#' )
+   scrContent.text.replaceAll( '#', 'fff' )
   writeFile file : "ff" , text : scrContent
 }
