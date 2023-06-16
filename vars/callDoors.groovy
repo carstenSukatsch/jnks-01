16e95db34502a1fb7abba7cc4555d398e79af97d
carsten sukatsch <121088867+carstenSukatsch@users.noreply.github.com>
2023-06-16 18:20:36 +0200

    Update callDoors.groovy

--- vars/callDoors.groovy
+++ vars/callDoors.groovy
@@ -1,1 +1,13 @@
-asdasdasdd
+
+def call( Map cfg = [:] ){
+
+  echo " herster param: ${cfg.name}"
+  def pp = ${cfg.name}
+  echo "${pp}"
+  
+  bat '''
+    @echo OFF
+    echo "call something "
+  '''
+  
+}
