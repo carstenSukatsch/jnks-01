7fc71f0628837a914380215587cf9a7c054873c3
carsten sukatsch <121088867+carstenSukatsch@users.noreply.github.com>
2023-06-23 22:56:19 +0200

    Update xFun.groovy

--- vars/xFun.groovy
+++ vars/xFun.groovy
@@ -1,10 +1,12 @@
 String  call(  ){
 
+ def bt = xf2()
  def  pfade = [
-"C:\\Program Files\\IBM\\Rational\\DOORS\\9.7\\bin",
-"\\\\afra-svdoorsfs\\DoorsHome97\$\\DAs_RM_DOORS_Extensions\\batch\\jobs\\ciBatch\\bt",
-"\\\\afra-svdoorsfs\\DoorsHome97\$\\DAs_RM_DOORS_Extensions\\batch\\jobs\\ciBatch\\state3JUnit"
+"C:\\Program Files\\IBM\\Rational\\DOORS\\9.7\\bin"
  ]
+ 
+pfade.addAll( bt )
+ 
  
  return  pfade.join(';')
 }
