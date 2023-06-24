26335ca41bda8dc29c3d2305d5dd667fb6eeb30f
carsten sukatsch <121088867+carstenSukatsch@users.noreply.github.com>
2023-06-24 11:55:11 +0200

    Update xFun.groovy

--- vars/xFun.groovy
+++ vars/xFun.groovy
@@ -1,9 +1,9 @@
 String  call(  ){
 
  String bt = xf2()
- 
+ String b2 = "C:\\Program Files\\IBM\\Rational\\DOORS\\9.7\\bin"
  def  pfade = [
-"C:\\Program Files\\IBM\\Rational\\DOORS\\9.7\\bin"
+     b2
  ]
  
 pfade.addAll( bt )
